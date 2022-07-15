1f8fbba710b85de29e92b4bec1389b9b9b2ab2e1
Bill La <bender@spinferno.com>
2022-07-15 23:12:36 +1000

    copy update

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -116,13 +116,6 @@ export const IndexPageTemplate = ({
                   </div>
                 </div>
                 <Features gridItems={intro.blurbs} />
-                {/* <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all projects
-                    </Link>
-                  </div>
-                </div> */}
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest posts
